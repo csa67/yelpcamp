1a656215cdb646b7588af1d9685532dcc2defb94
csa67 <saiaarathy@gmail.com>
2024-06-02 09:03:54 -0400

    serving statics

--- app.js
+++ app.js
@@ -26,6 +26,7 @@ app.set('view engine', 'ejs');
 
 app.use('/campgrounds', campgroundsRoute)
 app.use('/campgrounds/:id/reviews', reviewRoute)
+app.use(express.static(path.join(__dirname, 'public')))
 
 app.get("/", (req, res) => {
     res.render('home', { title: "YelpCamp" })
